0c4451068521d1ecce9a23d3efbf0986f87c2d2f
Maria Luísa <49032599+marialuisamol@users.noreply.github.com>
2023-05-29 23:39:27 -0300

    v8 - pequenas modificações

--- util.h
+++ util.h
@@ -8,10 +8,9 @@ void separa_pipe(char* linha_comando);
 void separa_maior(char* linha_comando);
 void separa_menor(char* linha_comando);
 char* getln();
-void pipe_simples(char **comandos);
 void inicializar();
 void inserir(char* comando);
-//void pipe_rec(char* comando);
+void pipe_complexo(char **comandos);
 void redirecionamento_maior(char** comandos, int* fd1);
 void redirecionamento_menor(char** comandos, int* fd1);
 #endif
